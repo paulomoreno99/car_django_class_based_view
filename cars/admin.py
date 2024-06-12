fe9bdbcb03c65a208014c6fba80373d002f8b545
paulomoreno99 <paulom.info@gmail.com>
2024-06-12 16:12:49 -0300

    aula_65

---
+++ cars/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from cars.models import Brand, Car
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model','brand')
+
+admin.site.register(Car, CarAdmin)
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Brand, BrandAdmin)
---
+++ cars/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import redirect, render
+from django.views import View
+from cars.models import Car
+from cars.forms import CarModelForm
+
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            cars = cars.filter(model__icontains=search)
+
+        return render(
+            request,
+            'cars.html',
+            {'cars': cars })
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()  
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+       
+
